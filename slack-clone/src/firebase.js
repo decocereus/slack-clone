40c76c818d9760f273e2858b9dc96c8726087f40
decocereus <amartyasinghkings07@gmail.com>
2021-07-26 18:46:16 +0530

    integrated firebase, redux and completed sidebar

--- slack-clone/src/firebase.js
+++ slack-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD7vnz-Y5WXOcIc2w8dk9fmhJ85GevYcLs",
@@ -8,3 +10,10 @@ const firebaseConfig = {
   appId: "1:966272668238:web:211165e34dfb48a604691f",
   measurementId: "G-KQ8MJ5H68J",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
+export const auth = firebase.auth();
+
+export const provider = new firebase.auth.GoogleAuthProvider();
