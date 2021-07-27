7eaed3a8ec50519dc8b6ab2e908772625a05ce87
decocereus <amartyasinghkings07@gmail.com>
2021-07-27 16:41:55 +0530

    done

--- slack-clone/src/firebase.js
+++ slack-clone/src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7vnz-Y5WXOcIc2w8dk9fmhJ85GevYcLs",
-  authDomain: "slack-clone-6c09b.firebaseapp.com",
-  projectId: "slack-clone-6c09b",
-  storageBucket: "slack-clone-6c09b.appspot.com",
-  messagingSenderId: "966272668238",
-  appId: "1:966272668238:web:211165e34dfb48a604691f",
-  measurementId: "G-KQ8MJ5H68J",
+  addYourOwn: "Add your own firebase config",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
